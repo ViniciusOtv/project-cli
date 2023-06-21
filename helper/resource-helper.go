815b4c15f40942da8eeff7be41e7742400ce2760
Vinicius Otavio <vinicius.soares@iterative.com.br>
2023-06-21 10:48:34 -0300

    Implementando novos arquivos .cs ao projeto. Criando método para instalar os pacotes do nuget

--- helper/resource-helper.go
+++ helper/resource-helper.go
@@ -35,8 +35,50 @@ func getNetCoreDefaultControllerFile() string {
 	return CurrentDirectory() + "/resources/netcore/BaseController.txt"
 }
 
-func getNetCoreSwaggerConfigurationFile() string {
-	return CurrentDirectory() + "/resources/netcore/SwaggerConfiguration.txt"
+func getNetCoreServiceFile() string {
+	return CurrentDirectory() + "/resources/netcore/Services/TelemetriaService.txt"
+}
+
+func getNetCoreInterfaceFile() string {
+	return CurrentDirectory() + "/resources/netcore/Services/Interfaces/ITelemetriaService.txt"
+}
+
+func getNetCoreBaseResponseFile() string {
+	return CurrentDirectory() + "/resources/netcore/Models/BaseResponse.txt"
+}
+
+func getNetCoreHealthCheckResponseFile() string {
+	return CurrentDirectory() + "/resources/netcore/Models/HealthCheckResponse.txt"
+}
+
+func getNetCoreConfigurationsFile() []string {
+	configurations := []string{CurrentDirectory() + "/resources/netcore/Configurations/SwaggerConfiguration.txt",
+		CurrentDirectory() + "/resources/netcore/Configurations/DependencyInjectionConfiguration.txt",
+		CurrentDirectory() + "/resources/netcore/Configurations/ErrorHandlingMiddlewareConfiguration.txt",
+		CurrentDirectory() + "/resources/netcore/Configurations/HealthCheckConfiguration.txt",
+		CurrentDirectory() + "/resources/netcore/Configurations/LogConfiguration.txt",
+		CurrentDirectory() + "/resources/netcore/Configurations/TelemetriaConfiguration.txt"}
+
+	return configurations
+}
+
+func replaceWordInConfigurationsFile(src []string, pathname string, old string, solutionName string) {
+	for _, value := range src {
+		filename := pathname + "/" + solutionName + "/Configurations/" + getRawFileName(value) + ".cs"
+
+		input, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		output := bytes.Replace(input, []byte(old), []byte(solutionName), -1)
+
+		if err = ioutil.WriteFile(filename, output, 0666); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
 }
 
 func replaceWordInFile(filename string, old string, new string) {
